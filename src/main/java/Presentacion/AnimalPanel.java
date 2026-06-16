97414aef2dac8b04e6b47e8f647de166410eabd7
yordan <yordanandresquiros@gmail.com>
2026-06-15 20:52:09 -0500

    limpiando codigo   v2

---
+++ src/main/java/Presentacion/AnimalPanel.java
@@ -0,0 +1,107 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JPanel.java to edit this template
+ */
+package Presentacion;
+
+import Aplicacion.ServiceImpl.AnimalServiceImpl;
+import Dominio.Modelo.Animal;
+import Dominio.Service.AnimalService;
+
+import javax.swing.table.DefaultTableModel;
+import java.util.List;
+
+/**
+ *
+ * @author yordan aquiles andres quiroz
+ */
+public class AnimalPanel extends javax.swing.JPanel {
+
+    private final AnimalService animalService;
+
+    // inyectamos la dependencia de AnimalService
+    public AnimalPanel() {
+        initComponents();
+        this.animalService = new AnimalServiceImpl();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated
+    // Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        btnListar = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tbAnimales = new javax.swing.JTable();
+
+        btnListar.setText("listar");
+        btnListar.addActionListener(this::btnListarActionPerformed);
+
+        tbAnimales.setModel(new javax.swing.table.DefaultTableModel(
+                new Object[][] {
+                        { null, null, null, null },
+                        { null, null, null, null },
+                        { null, null, null, null },
+                        { null, null, null, null }
+                },
+                new String[] {
+                        "Title 1", "Title 2", "Title 3", "Title 4"
+                }));
+        jScrollPane1.setViewportView(tbAnimales);
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+                jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(14, 14, 14)
+                                .addComponent(btnListar)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 75,
+                                        Short.MAX_VALUE)
+                                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 452,
+                                        javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addContainerGap()));
+        jPanel1Layout.setVerticalGroup(
+                jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(btnListar)
+                                .addGap(100, 100, 100))
+                        .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 361,
+                                        javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(0, 25, Short.MAX_VALUE)));
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE,
+                                javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE));
+        layout.setVerticalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE,
+                                javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE));
+    }// </editor-fold>//GEN-END:initComponents
+
+    // Accion ---> presiono el boton de listar
+    private void btnListarActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_btnListarActionPerformed
+        List<Animal> animales = animalService.finAll();
+        DefaultTableModel model = (DefaultTableModel) tbAnimales.getModel();
+        for (Animal animal : animales) {
+            model.addRow(new Object[] {
+                    animal.getIdAnimal(),
+                    animal.getEspecie(),
+                    animal.getRaza() });
+        }
+
+    }// GEN-LAST:event_btnListarActionPerformed
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnListar;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable tbAnimales;
+    // End of variables declaration//GEN-END:variables
+}
