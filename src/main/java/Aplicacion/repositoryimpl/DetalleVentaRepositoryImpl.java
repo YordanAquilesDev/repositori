7b85d2a2277aa553c922ef5de342bb064ecb5797
Neker23 <nekersanchez9@gmail.com>
2026-06-23 11:32:58 -0500

    Añadí datalleVenta y Movimiento en los Servicios

--- src/main/java/Aplicacion/repositoryimpl/DetalleVentaRepositoryImpl.java
+++ src/main/java/Aplicacion/repositoryimpl/DetalleVentaRepositoryImpl.java
@@ -1,4 +1,107 @@
 package Aplicacion.repositoryimpl;
 
-public class DetalleVentaRepositoryImpl {
+import Dominio.Modelo.DetalleVenta;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+public class DetalleVentaRepositoryImpl implements DetalleVentaRepository {
+
+    @Override
+    public int save(DetalleVenta detalleVenta) {
+
+        int respuesta = -1;
+        Connection conexion = null;
+        PreparedStatement preparar = null;
+
+        try {
+
+            String sql = """
+                    INSERT INTO detalle_venta
+                    (id_venta,id_producto,cantidad,subtotal)
+                    VALUES (?,?,?,?)
+                    """;
+
+            preparar = conexion.prepareStatement(sql);
+
+            preparar.setInt(1, detalleVenta.getVenta().getIdVenta());
+            preparar.setInt(2, detalleVenta.getProducto().getIdProducto());
+            preparar.setInt(3, detalleVenta.getCantidad());
+            preparar.setDouble(4, detalleVenta.getSubtotal());
+
+            respuesta = preparar.executeUpdate();
+
+            return respuesta;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+
+            try {
+
+                if (preparar != null) preparar.close();
+                if (conexion != null) conexion.close();
+
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    @Override
+    public int update(DetalleVenta detalleVenta) {
+
+        int respuesta = -1;
+        Connection conexion = null;
+        PreparedStatement preparar = null;
+
+        try {
+
+            String sql = """
+                    UPDATE detalle_venta
+                    SET id_venta = ?,
+                        id_producto = ?,
+                        cantidad = ?,
+                        subtotal = ?
+                    WHERE id_detalle = ?
+                    """;
+
+            preparar = conexion.prepareStatement(sql);
+
+            preparar.setInt(1, detalleVenta.getVenta().getIdVenta());
+            preparar.setInt(2, detalleVenta.getProducto().getIdProducto());
+            preparar.setInt(3, detalleVenta.getCantidad());
+            preparar.setDouble(4, detalleVenta.getSubtotal());
+            preparar.setInt(5, detalleVenta.getIdDetalle());
+
+            respuesta = preparar.executeUpdate();
+
+            return respuesta;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+
+            try {
+
+                if (preparar != null) preparar.close();
+                if (conexion != null) conexion.close();
+
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    @Override
+    public int delete(DetalleVenta detalleVenta) {
+        return 0;
+    }
+
+    @Override
+    public List<DetalleVenta> listarDetalleVentaPorId(Integer id) {
+        return List.of();
+    }
 }
